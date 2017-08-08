fb5decfce8b21ceeec7832afb742383d60298ff5
Oscar Elfving <oel@e-conomic.com>
2017-08-08 22:49:12 +0200

    recursive get of pages, recursive get of all verbs

--- scripts/importverbs.js
+++ scripts/importverbs.js
@@ -5,7 +5,7 @@ var wiktionaryApiUrl = 'https://sv.wiktionary.org/w/api.php'
 // ?action=query&list=categorymembers&
 //cmtitle=Kategori:Svenska/Verb&format=json&cmlimit=500'
 
-var getVerb = function(page) {
+var getVerb = function (page) {
   request({
     uri: wiktionaryApiUrl,
     qs: {
@@ -20,47 +20,68 @@ var getVerb = function(page) {
       console.log('Got error: ' + error)
       return
     }
-    $ = cheerio.load(body.parse)
-    var findVerb = $('table[class="grammar template-sv-verb-ar"]')
-    .find("th:contains('Imperativ')")
-    .find('a[class="mw-selflink selflink"')
-    console.log(findVerb)
+
+    var pageObject = JSON.parse(body)
+
+    $ = cheerio.load(pageObject.parse.text)
+
+    var findVerb = $("th:contains('Imperativ')")
+      .siblings('td')
+      .find('a[class="mw-selflink selflink"]').text()
+
+    if (findVerb == "") return
+
+
+    console.log("Found imperative verb: " + findVerb)
+    saveVerb(findVerb)
   })
 }
 
-var saveVerb = function(verb) {
+var saveVerb = function (verb) {
 
 }
 
-var getNextPage = function(continuetoken) {
+var getNextPage = function (continuetoken) {
 
 }
 
-request({
-  uri: wiktionaryApiUrl,
-  qs: {
-    action: 'query',
-    list: 'categorymembers',
-    cmtitle: 'Kategori:Svenska/Verb',
-    format: 'json',
-    cmlimit: 10
-    }}, function(error, response, body) {
-  var cmcontinue = ''
-  if(error) {
-    console.log(error)
-    return
+var getVerbs = function (continuetoken) {
+  if (!continuetoken) {
+    var continuetoken = ''
   }
-  console.log('Got statuscode ' + response.statusCode)
-  var returnObject = JSON.parse(body)
-  if(returnObject.continue) {
-    if(returnObject.continue.cmcontinue) {
-      cmcontinue = returnObject.continue.cmcontinue
-      console.log('Got cmcontinue token: ' + cmcontinue)
+  request({
+    uri: wiktionaryApiUrl,
+    qs: {
+      action: 'query',
+      list: 'categorymembers',
+      cmtitle: 'Kategori:Svenska/Verb',
+      format: 'json',
+      cmlimit: 500,
+      cmcontinue: continuetoken
     }
-  }
-  
-  returnObject.query.categorymembers.forEach(function (val) {
-    getVerb(val)
-  })
+  }, function (error, response, body) {
+    var cmcontinue = ''
+    if (error) {
+      console.log(error)
+      return
+    }
+    console.log('Got statuscode ' + response.statusCode)
+    var returnObject = JSON.parse(body)
+    if (returnObject.continue) {
+      if (returnObject.continue.cmcontinue) {
+        cmcontinue = returnObject.continue.cmcontinue
+        console.log('Got cmcontinue token: ' + cmcontinue)
+      }
+    }
 
-})
+    returnObject.query.categorymembers.forEach(function (val) {
+      setTimeout(getVerb,500,val)
+    })
+    if(cmcontinue != '') {
+      setTimeout(getVerbs, 5000, cmcontinue )
+      
+    }
+  })
+}
+
+getVerbs()
